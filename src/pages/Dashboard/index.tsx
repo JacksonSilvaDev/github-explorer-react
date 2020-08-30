081f832a2d58958a9544197af26884c522ff457b
Jackson Silva <jacksonsilvadev@gmail.com>
2020-08-30 16:25:56 -0300

    Added a style to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,56 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit"> Pesquisar </button>
+      </Form>
+      <Repositories>
+        <a href="a">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43053055?s=460&u=159d1c86df5d6911c14e8d60d0a9b7f19a675588&v=4"
+            alt="Jackson Silva"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms !</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="a">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43053055?s=460&u=159d1c86df5d6911c14e8d60d0a9b7f19a675588&v=4"
+            alt="Jackson Silva"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms !</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="a">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43053055?s=460&u=159d1c86df5d6911c14e8d60d0a9b7f19a675588&v=4"
+            alt="Jackson Silva"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms !</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
